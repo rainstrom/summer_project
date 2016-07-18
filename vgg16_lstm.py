b10d37970fb5613b074e5680045be6274fb652a6
Xiaoyang Guo <xiaoyang@compute-1-24.local>
2016-07-18 00:59:20 -0400

    modi

--- vgg16_lstm.py
+++ vgg16_lstm.py
@@ -2,14 +2,13 @@ import tensorflow as tf
 import numpy as np
 from layers import *
 import vgg16
-from tf.models.
 
 def inference(input, batch_size, num_segments, lstm_keep_prob=0.5, conv_keep_prob=1.0, train_conv123=False, train_conv45=False, train_fc67=False):
     import pdb; pdb.set_trace()
     with tf.variable_scope("conv"):
         fc8 = vgg16.inference(input, conv_keep_prob, train_conv123, train_conv45, train_fc67)
-        fc7 = tf.get_default_graph().get_tensor_by_name("fc7")
-        fc6 = tf.get_default_graph().get_tensor_by_name("fc6")
+        fc7 = tf.get_default_graph().get_tensor_by_name("conv/fc7/conv/fc7:0")
+        fc6 = tf.get_default_graph().get_tensor_by_name("conv/fc6/conv/fc6:0")
         # output is [batch_size*num_segments, 4096]
     with tf.variable_scope("lstm"):
         batch_size = 20
