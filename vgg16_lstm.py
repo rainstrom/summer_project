a5424956814afc86db67c3c33b6b271eb0ed5a08
Xiaoyang Guo <xiaoyang.guo1995@gmail.com>
2016-07-17 23:46:27 -0400

    lstm

--- vgg16_lstm.py
+++ vgg16_lstm.py
@@ -2,29 +2,33 @@ import tensorflow as tf
 import numpy as np
 from layers import *
 import vgg16
+from tf.models.
 
 def inference(input, lstm_keep_prob=0.5, conv_keep_prob=1.0, train_conv123=False, train_conv45=False, train_fc67=False):
+    import pdb; pdb.set_trace()
     with tf.variable_scope("conv"):
-        fc7 = vgg16.inference(input, conv_keep_prob, train_conv123, train_conv45, train_fc67)
+        fc8 = vgg16.inference(input, conv_keep_prob, train_conv123, train_conv45, train_fc67)
+        fc7 = tf.get_default_graph().get_tensor_by_name("fc7")
         fc6 = tf.get_default_graph().get_tensor_by_name("fc6")
-        fc5 = tf.get_default_graph().get_tensor_by_name("fc5")
+        # output is [batch_size*num_segments, 4096]
     with tf.variable_scope("lstm"):
-        lstm_state_size = 256
-        batch_size = fc6.get_shape()[0].value
-        lstm = rnn_cell.BasicLSTMCell(lstm_size)
-        state = tf.zeros([batch_size, lstm_state_size])
-        # Initial state of the LSTM memory.
-        state = tf.zeros([batch_size, lstm.state_size])
+        batch_size = 20
+        num_segments = 25
+        hidden_size = 256
+        lstm_input = tf.reshape(fc7, [batch_size, num_segments, 4096])
+        lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(hidden_size, forget_bias=1.0)
+        lstm_cell = tf.nn.rnn_cell.DropoutWrapper(lstm_cell, output_keep_prob=lstm_keep_prob)
+        cell = tf.nn.rnn_cell.MultiRNNCell([lstm_cell] * 2)
+        _initial_state = cell.zero_state(batch_size, tf.float32)
 
-        loss = 0.0
-        for current_batch_of_words in words_in_dataset:
-            # The value of state is updated after processing each batch of words.
-            output, state = lstm(current_batch_of_words, state)
+        outputs = []
+        for time_step in range(num_segments):
+            if time_step > 0: tf.get_variable_scope().reuse_variables()
+            (cell_output, state) = cell(inputs[:, time_step, :], state)
+            outputs.append(cell_output)
+        
 
-            # The LSTM output can be used to make next word predictions
-            logits = tf.matmul(output, softmax_w) + softmax_b
-            probabilities = tf.nn.softmax(logits)
-            loss += loss_function(probabilities, target_words)
+        final_state = state
     return logits
 
 def loss(logits, labels, weight_decay=0.001):
